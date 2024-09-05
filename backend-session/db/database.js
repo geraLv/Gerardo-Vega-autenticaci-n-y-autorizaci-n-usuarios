accbe203fcaf4ce506cd0b3c6de42ed5eb987d7f
gerardo vega <gerardoluisvega@gmail.com>
2024-09-05 14:46:18 -0300

    Se modulariso la rama backend-session y se creo la ruta register

--- backend-session/db/database.js
+++ backend-session/db/database.js
@@ -1,25 +1,15 @@
-export const database = 
-    {
-        user: [
-            {
-                id: 1,
-                username: 'admin',
-                password: 'admin'
-            },
-            {
-                id: 2,
-                username: 'user',
-                password: 'user'
-            },
-            {
-                id: 3,
-                username: 'guest',
-                password: 'guest'
-            },
-            {
-                id: 4,
-                username: 'guest2',
-                password: 'guest2'
-            }
-        ]
-    }
+import {createPool}  from "mysql2/promise"
+
+const createMyPool = async (req, res)=>{
+    const coso = createPool({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "db_system"
+    });
+    return coso
+        
+};
+const pool = await createMyPool()
+
+export { pool }
